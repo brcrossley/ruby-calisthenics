51c4dedff5156572a1d34083ae6cc1af53d099ee
Bryce <bcrossley@unomaha.edu>
2017-06-03 22:20:10 +0000

    word count

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -4,10 +4,11 @@ module FunWithStrings
     word = word.gsub(/(\W|\d)/, "")
     word == word.reverse
   end
-  def count_words(s)
-    words = s.split(/\b/)
-    count = Hash.new
+  def count_words
+    words = self.split(/\W+/)
+    count = Hash.new(0)
     words.each { |w| count[w.downcase] += 1 }
+    count.sort_by { |k,v| v }.reverse
     return count
   end
   def anagram_groups
