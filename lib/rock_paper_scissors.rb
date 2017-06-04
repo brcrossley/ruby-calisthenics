b8d8fcd558f204b65219e6551bcd0c61098a7b44
Bryce <bcrossley@unomaha.edu>
2017-06-04 18:56:53 +0000

    rps winner now functions

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,10 +4,9 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    round = "#{player1[1]}#{player2[1]}" #combines the two strategies into one string for easy handling
-    if round == "/(PR)|(RS)|(SP)/"
+    if (player1[1] == "P" and player2[1] == "R") or (player1[1] == "R" and player2[1] == "S") or (player1[1] == "S" and player2[1] == "P") or (player1[1] == "R" and player2[1] == "R") or (player1[1] == "S" and player2[1] == "S") or (player1[1] == "P" and player2[1] == "P")
 	player1
-    elsif round == "/(RP)|(SR)|(PS)/"
+    elsif (player1[1] == "R" and player2[1] == "P") or (player1[1] == "S" and player2[1] == "R") or (player1[1] == "P" and player2[1] == "S")
 	player2
     else
 	raise NoSuchStrategyError, "Strategy must be one of R,P,S"
@@ -15,7 +14,10 @@ class RockPaperScissors
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if tournament[0][0]
+ 	winner(tournament[0],tournament[1])
+    else
+	winner(tournament_winner(tournament[0]),tournament_winner(tournament[1]))
+    end
   end
-
 end
