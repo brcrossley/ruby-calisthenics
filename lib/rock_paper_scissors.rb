87863c9f52120b05f1c061c751385515a04db129
Bryce <bcrossley@unomaha.edu>
2017-06-04 17:44:22 +0000

    fixed NameError in rps

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,13 +4,13 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    raise NoSuchStrategyError, "Strategy must be one of R,P,S" unless player1[1] == "/[RPS]/" and player2[1] == "/[RPS]/"
-
-    round = player[1] + player2[1] #combines the two strategies into one string for easy handling
+    round = "#{player1[1]}#{player2[1]}" #combines the two strategies into one string for easy handling
     if round == "/(PR)|(RS)|(SP)/"
 	player1
-    else
+    elsif round == "/(RP)|(SR)|(PS)/"
 	player2
+    else
+	raise NoSuchStrategyError, "Strategy must be one of R,P,S"
     end
   end
 
